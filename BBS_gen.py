cae2b934c0b6969b8e6af0766e9886a074bf1bba
Pietro <p.valese@studenti.unipi.it>
2024-10-02 15:32:41 +0200

    Fix and update

--- BBS_gen.py
+++ BBS_gen.py
@@ -2,22 +2,25 @@ from Test_Miller_Rabin import test_MR
 from Test_Miller_Rabin import MCD
 import random
 
+RANGE_START=10**20
+RANGE_END=10**40
+
 # Function to generate two large primes p and q, where p ≡ 3 (mod 4) and q ≡ 3 (mod 4)
 def gen_pq():
-    p=random.randint(100000, 1000000000)
-    q=random.randint(100000, 1000000000)
+    p=random.randint(RANGE_START,RANGE_END)
+    q=random.randint(RANGE_START,RANGE_END)
     n1=2*(p//4)+1
     n2=2*(q//4)+1
     while test_MR(n1,10) == False or test_MR(n2,10) == False:
         # Randomly generate large odd numbers
-        p = random.randint(100000, 1000000000)
+        p = random.randint(RANGE_START,RANGE_END)
         if p % 2 == 0:
             p += 1
         # Ensure p ≡ 3 (mod 4) and that p is prime
         while not (p % 4 == 3 and test_MR(p, 10)):
             p += 2
         
-        q = random.randint(100000, 1000000000)
+        q = random.randint(RANGE_START,RANGE_END)
         if q % 2 == 0:
             q += 1
         # Ensure q ≡ 3 (mod 4) and that q is prime
@@ -26,19 +29,17 @@ def gen_pq():
             
         n1=2*(p//4)+1
         n2=2*(q//4)+1
-        # Return the product of the two primes
-    print(p,q)
+    # Return the product of the two primes
     return p * q
 
 # Function to generate a Blum Blum Shub pseudorandom sequence of length n
 def BBS(n):
     # Generate Blum integer (product of two primes p and q, where p ≡ 3 (mod 4) and q ≡ 3 (mod 4))
     num = gen_pq()
-    # Choose a random seed y such that GCD(y, num) == 1
-    y = random.randint(100000, 1000000000)
+    # Choose a random seed y such that MCD(y, num) == 1
+    y = random.randint(RANGE_START,RANGE_END)
     while MCD(y, num) != 1:
         y += 1
-    print(y)
     # Initialize the BBS sequence
     x = (y ** 2) % num
     sequence = [x]
--- linear_generator.py
+++ linear_generator.py
@@ -1,23 +1,17 @@
 import random
-
+MODULUS=2**32
 # Linear Congruential Generator (LCG)
-def linear_generator(n, modulus=2**32, a=None, b=None, seed=None):
+def linear_generator(n):
     # Generate random parameters if not provided
-    if a is None:
-        a = random.randint(1, modulus - 1)
-    if b is None:
-        b = random.randint(0, modulus - 1)
-    if seed is None:
-        seed = random.randint(0, modulus - 1)
-
+    a = random.randint(1, MODULUS - 1)
+    b = random.randint(0, MODULUS - 1)
+    seed = random.randint(0, MODULUS - 1)
     seq = []
     seq.append(seed)
-    
     for i in range(1, n):
-        # LCG recurrence relation: X_{n+1} = (a * X_n + b) % modulus
-        next_value = (a * seq[i - 1] + b) % modulus
+        # LCG recurrence relation: X_{n+1} = (a * X_n + b) % MODULUS
+        next_value = (a * seq[i - 1] + b) % MODULUS
         seq.append(next_value)
-    
     return seq
 
 if __name__ == "__main__":
